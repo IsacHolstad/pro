1705322fc064dff010f416b79dc8f060fe4e2521
IsacHolstad <wisac22@gmail.com>
2022-01-10 14:04:17 +0100

    1 lecture on moday done

--- java.js
+++ java.js
@@ -1,19 +1,67 @@
 var animal = "bruh";
 console.log(animal);
 //warm up
-let fruits = ('Apple', 'Banana')
-console.log(fruits.length)
-//2
-let first = fruits(0)
-//apple
-let last = fruits(fruits.length - 1)
-//banana
-fruits.forEach(function(item, index, array) {
-    console.log(item, index)
-})
-//apple 0
-//banana 1
-let newLength = fruits.push('Orange')
-//("Apple", "Banana", "Orange",)
-let last = fruits.pop() //remove orange (from the enter) 
-// ("apple", "banana")
+
+// (&&) in the slides of and operator
+//var selectedNumber = 60;
+//if selectedNumber = 30;
+//if (selectedNumber >= )
+
+// or in the slides
+//var dayOfTheWeekend = "saturday";
+
+//homework
+var stringFour = "4";
+var numberFour = 4;
+
+//lecture 1 on monday
+if (stringFour === numberFour){
+    console.log("equal");
+}
+else {
+    console.log("not equal");
+}
+//correct the answer in the console comes back "not equal"
+var selectedNumber = 30;
+if (selectedNumber >= 10 && selectedNumber <= 50){
+    console.log("The Selected Number is within range");
+}
+//aswer came "5" witch is correct
+
+var dayOfTheWeek = "Saturday";
+if (dayOfTheWeek === "Saturday" || dayOfTheWeek ===
+"Sunday") {
+    console.log("it's the weekend");
+}
+//came back as nothing *fix it*
+
+//undenified a null
+//if  variable (variable === null) {
+    //console.log("aw man no data!");
+//}
+var stringFour = "four"
+var numberFour = 4
+if (stringFour === numberFour) {
+    console.log("equal");
+} else {
+    console.log("not equal")
+}
+// not equal!!!
+
+
+
+
+
+
+
+
+
+
+
+
+//lecture 2 on monday
+var someArray = ["red", "blue", "green", "yellow"];
+someArray.push("isac");
+console.log(someArray);
+//someArray.pop();
+console.log(someArray.length);
