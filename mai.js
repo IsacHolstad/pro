a4c88a147a19e68dfbc96434dcfe8a893854d51a
IsacHolstad <wisac22@gmail.com>
2022-05-27 14:39:47 +0200

    trying filter seacrh

--- mai.js
+++ mai.js
@@ -32,7 +32,7 @@ async function blogResults() {
             blogPost.innerHTML += `<div class="postcontainer"><a href="detail.html?id=${responseJSON[i].id}">
            
             </div>
-            <h3> ${responseJSON[i].slug}</h3>
+            <h3 class="blog-title">${responseJSON[i].slug}</h3>
             <div class="img-blog-posts"> ${wholeResponse[i].content.rendered}</div>
             <center><button class="read-more-btn">Read More</button></center>
             `
@@ -1148,6 +1148,22 @@ for (let i = 0; i < wholeResponse.length; i++) {
 
     }
     console.log(wholeResponse[i].content.rendered)
+}
+function myFunction() {
+    var input, filter, ul, li, a, i, h3, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("blog-title");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
 
 
